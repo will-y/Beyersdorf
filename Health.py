b7d0ef7a39fc6ba8a986490af14e7db71d9673a2
Will Yelton <yeltonwh@rose-hulman.edu>
2017-06-20 13:56:08 -0400

    Added Health

--- Health.py
+++ Health.py
@@ -1,10 +1,14 @@
 import math
 import pygame as pg
+import Terrain
 
 """Class that deals with health of buildings"""
 class Health():
-    def __init__(self, health, position):
-        pass
+    def __init__(self, health, position, tileSize):
+        self.health = health
+        self.position = position
 
+    """Draws the health onto a tile givin percentage of health"""
     def drawHealth(self, percentOfTotalHealth):
-        pass
+        pg.draw.rect(self.screen, pg.Color("black"), (position.x + 15, position.y + 75, 70, 15), 2)
+        pg.draw.rect(self.screen, pg.Color("green"), (position.x + 15, position.y + 75, 70, 15))
