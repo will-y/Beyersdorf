4c659034fbea86acd285df2a940255a18dfcdb32
Will Yelton <yeltonwh@rose-hulman.edu>
2017-06-20 15:19:13 -0400

    Added detect click function

--- Main.py
+++ Main.py
@@ -12,6 +12,16 @@ class Main():
         self.background = self.background.convert()
         self.screen.fill(pg.Color('white'))
         self.clock = pg.time.Clock()
+        self.xCoord = 0
+        self.yCoord = 0
+
+    def detectClick(self):
+        #for event in pg.event.get():
+        if(pg.mouse.get_pressed()[0]):
+            self.xCoord = (int)(pg.mouse.get_pos()[0]/100)
+            self.yCoord = (int)(pg.mouse.get_pos()[1]/100)
+
+            return (self.xCoord, self.yCoord)
 
     def runGame(self):
         terrainobject = Terrain.Terrain(10, self.width)
@@ -25,6 +35,8 @@ class Main():
                     sys.exit()
             pg.mouse.set_cursor(*pg.cursors.broken_x)
             pg.display.update()
+            #print(self.detectClick())
+            #sys.stdout.flush()
 
 def main():
     pg.init()
