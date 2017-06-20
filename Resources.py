18c57d62c49a2bbc0f5615fef7a62425bb38be44
kids <buccelgt@rose-hulman.edu>
2017-06-20 12:39:14 -0500

    stuff

--- Resources.py
+++ Resources.py
@@ -2,4 +2,9 @@ import math
 import time
 
 class Resources():
-    def __init__()
+    def __init__(self,type,amount):
+        self.type = type #TYPE OF RESOURCE IE: WOOD, STONE, IRON, etc
+        self.amount = amount
+    
+    def stuff(self,theAmount): #ADDS RESOURCES TO AMOUNT STORED AS THE CURRENT RESOURCE
+        self.amount+=theAmount
