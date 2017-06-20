23e6640b590fb61c80050f2aae7a9d8acacbd4b8
Will Yelton <yeltonwh@rose-hulman.edu>
2017-06-20 13:29:33 -0400

    Started Health.py

---
+++ Health.py
@@ -0,0 +1,10 @@
+import math
+import pygame as pg
+
+"""Class that deals with health of buildings"""
+class Health():
+    def __init__(self, health, position):
+        pass
+
+    def drawHealth(self, percentOfTotalHealth):
+        pass
