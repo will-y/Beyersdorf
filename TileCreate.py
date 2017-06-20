5e4aa8d48ad08b172a69d53cd0d64edaea95ba4e
Nicholas Scarfe <scarfeng@rose-hulman.edu>
2017-06-20 15:28:03 -0400

    Fixed

--- TileCreate.py
+++ TileCreate.py
@@ -1,11 +1,8 @@
 import pygame as pg
 import time
 import math
-<<<<<<< HEAD
 import random
-=======
 import Health
->>>>>>> 4c659034fbea86acd285df2a940255a18dfcdb32
 
 """Creates tile"""
 class GenerateTile:
@@ -17,7 +14,7 @@ class GenerateTile:
         
 
     def generate_tile(self, x, y, tilesize, screen):
-        tile = pg.Rect((x*tilesize, y*tilesize),(tilesize, tilesize))
+        self.tile = pg.Rect((x*tilesize, y*tilesize),(tilesize, tilesize))
         pg.draw.rect(screen, pg.Color("black"), (x*tilesize, y*tilesize,tilesize, tilesize), 1)
         healthBar = Health.Health((x*tilesize, y*tilesize), 100, screen)
         healthBar.drawHealth(100, 100)
