2cbbadc810ecfb1bcb21c61f5c4e122d9567175d
kids <buccelgt@rose-hulman.edu>
2017-06-20 12:53:19 -0500

    minor changes (name of class Resource)

--- Resource.py
+++ Resource.py
@@ -1,10 +1,10 @@
 import math
 import time
 
-class Resources():
+class Resource():
     def __init__(self,type,amount):
         self.type = type #TYPE OF RESOURCE IE: WOOD, STONE, IRON, etc
         self.amount = amount
     
-    def stuff(self,theAmount): #ADDS RESOURCES TO AMOUNT STORED AS THE CURRENT RESOURCE
+    def edit(self,theAmount): #ADDS RESOURCES TO AMOUNT STORED AS THE CURRENT RESOURCE
         self.amount+=theAmount
