a8f29e41c1f733b67fd80461c4a93e85d6a37eca
kids <buccelgt@rose-hulman.edu>
2017-06-20 12:57:16 -0500

    Minor changes to TileCreate

--- TileCreate.py
+++ TileCreate.py
@@ -7,9 +7,12 @@ class GenerateTile:
 
     def __init__(self, tileType):
         self.tileType = tileType
-        self.tileResourceWood = 
-        self.tileResourceStone = 
-        self.tileResourceOre = 
+        self.tileResourceWood = 0
+        self.tileResourceStone = 0
+        self.tileResourceOre = 0
 
     def generate_tile(self, x, y, tilesize):
         tile = pg.rect((x*tilesize, y*tilesize),(tilesize, tilesize))
+
+    def generate_Resources(self,tileType):
+        pass
