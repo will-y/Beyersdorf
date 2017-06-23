43e81603af5ba74e7dddcb928d71105865ce474f
Nicholas Scarfe <scarfeng@rose-hulman.edu>
2017-06-23 15:54:19 -0400

    Fixed UI

--- UserInterface.py
+++ UserInterface.py
@@ -76,17 +76,11 @@ class UserInterface():
         self.playerFoodPerTurn = " +0/Turn"
 
         #Resource Amounts
-<<<<<<< HEAD
         self.woodCount = self.resourceCountFont.render("1000", True, pg.Color('black'))
         self.stoneCount = self.resourceCountFont.render("200", True, pg.Color('black'))
         self.oreCount = self.resourceCountFont.render("100", True, pg.Color('black'))
         self.foodCount = self.resourceCountFont.render("0", True, pg.Color('black'))
-=======
-        self.woodCount = self.resourceCountFont.render("1000" + self.playerWoodPerTurn, True, pg.Color('black'))
-        self.stoneCount = self.resourceCountFont.render("1000" + self.playerStonePerTurn, True, pg.Color('black'))
-        self.oreCount = self.resourceCountFont.render("100" + self.playerOrePerTurn, True, pg.Color('black'))
-        self.foodCount = self.resourceCountFont.render("0" + self.playerFoodPerTurn, True, pg.Color('black'))
->>>>>>> 19065ace400b296321f3bb11b9ad4cd1599af9f7
+
         self.populationCount = self.resourceCountFont.render("0 / 0", True, pg.Color('black'))
 
         #Background rectangle dimensions
