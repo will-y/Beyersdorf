e5ca6b5d4396933999b149270a2bdcfaccddd4b3
Grant Duchars <spam.th3.ham@gmail.com>
2017-06-23 13:59:23 -0400

    updated tiles and main menu

--- MainMenu.py
+++ MainMenu.py
@@ -10,62 +10,50 @@ class Main_Menu:
         self.startscreen = pg.display.set_mode((self.width, self.height))
         self.background = pg.Surface(self.startscreen.get_size())
         self.background = self.background.convert()
-        self.screen_color = (255,255,255)
+        self.screen_color = (128, 0, 0)
         self.clock = pg.time.Clock()
         # Game Screen
         self.title_width = None
         self.tile_height = None
         # Keys Pressed
         self.keys = []
-        # Fonts
-        self.titleFont = pg.font.SysFont("monospace", 50)
+        # Title
+        self.title = pg.image.load("Images/beyersdorfTitle.png")
+        self.title = pg.transform.scale(self.title, (533, 133))
+        # Info Text
+        self.font = pg.font.Font("OptimusPrinceps.ttf", 30)
+        self.text1 = self.font.render("Welcome to Beyersdörf!", 1, pg.Color("Black"))
+        self.text2 = self.font.render("Beyersdörf is a tile based strategy game.", 1, pg.Color("Black"))
+        self.text3 = self.font.render("The object of the game is to destroy the enemies base.", 1, pg.Color("Black"))
+        self.text4 = self.font.render("Gather resources by building buildings on tiles.", 1, pg.Color("Black"))
+        self.text5 = self.font.render("Use resources to build other buildings and create you empire.", 1, pg.Color("Black"))
+        self.text6 = self.font.render("Control the board and push into your enemies territory.", 1, pg.Color("Black"))
+        self.text7 = self.font.render("Take down thier base to win the game.", 1, pg.Color("Black"))
+        self.text8 = self.font.render("Good Luck!", 1, pg.Color("Black"))
+        self.text9 = self.font.render("(Press Enter to start the game)", 1, pg.Color("Black"))
 
     def runScreen(self):
         pg.init()
-        textBox = eztext.Input(maxlength=45, color=(0,0,0), prompt="Board Size: ")
+        pg.font.init()
+        # textBox = eztext.Input(maxlength=45, color=(0,0,0), prompt="Board Size: ")
         while True:
             self.clock.tick(10)
             events = pg.event.get()
             key = pg.key.get_pressed()
-            # Stores the key inputs
-            if key[pg.K_0]:
-                self.keys.append("0")
-                time.sleep(0.1)
-            if key[pg.K_1]:
-                self.keys.append("1")
-                time.sleep(0.1)
-            if key[pg.K_2]:
-                self.keys.append("2")
-                time.sleep(0.1)
-            if key[pg.K_3]:
-                self.keys.append("3")
-                time.sleep(0.1)
-            if key[pg.K_4]:
-                self.keys.append("4")
-                time.sleep(0.1)
-            if key[pg.K_5]:
-                self.keys.append("5")
-                time.sleep(0.1)
-            if key[pg.K_6]:
-                self.keys.append("6")
-                time.sleep(0.1)
-            if key[pg.K_7]:
-                self.keys.append("7")
-                time.sleep(0.1)
-            if key[pg.K_8]:
-                self.keys.append("8")
-                time.sleep(0.1)
-            if key[pg.K_9]:
-                self.keys.append("9")
-                time.sleep(0.1)
             if key[pg.K_RETURN]:
-                # self.startscreen.fill(self.screen_color)
-                # sys.exit()
                 break
             for event in events:
                 if event.type == pg.QUIT:
                     sys.exit()
             self.startscreen.fill(self.screen_color)
-            textBox.update(events)
-            textBox.draw(self.startscreen)
+            self.startscreen.blit(self.title, (133, 50))
+            self.startscreen.blit(self.text1, (50,200))
+            self.startscreen.blit(self.text2, (50,230))
+            self.startscreen.blit(self.text3, (50,260))
+            self.startscreen.blit(self.text4, (50,290))
+            self.startscreen.blit(self.text5, (50,320))
+            self.startscreen.blit(self.text6, (50,350))
+            self.startscreen.blit(self.text7, (50,380))
+            self.startscreen.blit(self.text8, (50,410))
+            self.startscreen.blit(self.text9, (50,470))
             pg.display.update()
